321d0a0654b1475a94a08b00b4bc801dd3430987
VolodymyrKumejko <volodymyr.kumejko@gmail.com>
2019-03-22 10:05:13 +0200

    Create HW2

---
+++ KVA_HW1/KVA_HW1/Shared/IFly.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KVA_HW1.Shared
+{
+    interface IFly
+    {
+    }
+}
